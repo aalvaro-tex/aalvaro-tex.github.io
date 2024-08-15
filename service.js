c51aac798985e4a47ea8d02a502bec5b78cec72a
gbxh14 <alvaro.gobernado@altia.es>
2024-08-15 13:01:44 +0200

    añadido js

---
+++ service.js
@@ -0,0 +1,17 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+function onOnlyFansClick(){
+    if(localStorage.getItem("num")){
+        alert("Eres la persona " + parseInt(localStorage.getItem("num"))+1 + " que hace click aquí!");
+        localStorage.setItem("num", parseInt(localStorage.getItem("num"))+1);
+    }
+    else
+    {
+        num = getRndInteger(1000,2000);
+        alert("Eres la persona " + num + " que hace click aquí!");
+        localStorage.setItem("num", num);
+    }
+
+}
