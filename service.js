718b67490065360dd68a52caf92c0869d22632cf
gbxh14 <alvaro.gobernado@altia.es>
2024-08-15 21:12:27 +0200

    añadido cv

--- service.js
+++ service.js
@@ -1,17 +1,15 @@
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-function onOnlyFansClick(){
-    if(localStorage.getItem("num")){
-        alert("Eres la persona " + (parseInt(localStorage.getItem("num"))+1) + " que hace click aquí!");
-        localStorage.setItem("num", parseInt(localStorage.getItem("num"))+1);
+function onOnlyFansClick() {
+    if (localStorage.getItem("num")) {
+        alert("Eres la persona " + (parseInt(localStorage.getItem("num")) + 1) + " que hace click aquí!");
+        localStorage.setItem("num", parseInt(localStorage.getItem("num")) + 1);
     }
-    else
-    {
-        num = getRndInteger(1000,2000);
+    else {
+        num = getRndInteger(1000, 2000);
         alert("Eres la persona " + num + " que hace click aquí!");
         localStorage.setItem("num", parseInt(num));
     }
-
 }
